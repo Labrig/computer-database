82e1981a09c74994c1a153f6809fd5c876ee8af1
Labrig <mgirbal@excilys.com>
2019-02-14 12:20:15 +0100

    [UPDATE] test manager executed in a test database

--- test/fr/excilys/business/CompanyManagerTest.java
+++ test/fr/excilys/business/CompanyManagerTest.java
@@ -1,19 +1,84 @@
 package fr.excilys.business;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.lang.reflect.Field;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import fr.excilys.model.Company;
 
 public class CompanyManagerTest {
 
+	private static CompanyManager manager = ManagerFactory.getInstance().getCompanyManager();
+	private static Field entityManagerField;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		try {
+			entityManagerField = ModelManager.class.getDeclaredField("entityManager");
+			entityManagerField.setAccessible(true);
+			entityManagerField.set(manager, Persistence.createEntityManagerFactory("computer-database-test").createEntityManager());
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
-	public void test() {
-		CompanyManager manager = ManagerFactory.getInstance().getCompanyManager();
+	public void testListCompanyEmpty() {
 		List<Company> listCompany = manager.listCompany();
-		for(Company company : listCompany) {
-			System.out.println(company.getName());
+		assertEquals(0, listCompany.size());
+	}
+	
+	@Test
+	public void testFindCompanyEmpty() {
+		Company company = manager.findCompany(1);
+		assertNull(company);
+	}
+	
+	@Test
+	public void testListAndFindCompanyWith1() {
+		insertNewTestCompany();
+		List<Company> listCompany = manager.listCompany();
+		assertEquals(1, listCompany.size());
+		Company company = manager.findCompany(listCompany.get(0).getId());
+		assertEquals(listCompany.get(0), company);
+		deleteAllCompany();
+	}
+
+	private void deleteAllCompany() {
+		try {
+			EntityManager entityManager = (EntityManager)entityManagerField.get(manager);
+			for(Object company : entityManager.createQuery("FROM Company").getResultList()) {
+				entityManager.getTransaction().begin();
+				entityManager.remove(company);
+				entityManager.getTransaction().commit();
+			}
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void insertNewTestCompany() {
+		try {
+			Company company = new Company();
+			company.setName("test");
+			EntityManager entityManager = (EntityManager)entityManagerField.get(manager);
+			entityManager.getTransaction().begin();
+			entityManager.persist(company);
+			entityManager.getTransaction().commit();
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
 		}
 	}
 
--- test/fr/excilys/business/ComputerManagerTest.java
+++ test/fr/excilys/business/ComputerManagerTest.java
@@ -1,39 +1,67 @@
 package fr.excilys.business;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.lang.reflect.Field;
 import java.util.List;
 
+import javax.persistence.Persistence;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import fr.excilys.model.Computer;
 
 public class ComputerManagerTest {
 
-	@Test
-	public void testFindComputer() {
-		ComputerManager manager = ManagerFactory.getInstance().getComputerManager();
-		Computer computer = manager.findComputer(1L);
-		System.out.println(computer.toString());
+	private static ComputerManager manager = ManagerFactory.getInstance().getComputerManager();
+	private static Field entityManagerField;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		try {
+			entityManagerField = ModelManager.class.getDeclaredField("entityManager");
+			entityManagerField.setAccessible(true);
+			entityManagerField.set(manager, Persistence.createEntityManagerFactory("computer-database-test").createEntityManager());
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
-	public void testCreateUpdateDeleteComputer() {
-		ComputerManager manager = ManagerFactory.getInstance().getComputerManager();
+	public void testListComputerEmpty() {
+		List<Computer> listComputer = manager.listComputer();
+		assertEquals(0, listComputer.size());
+	}
+	
+	@Test
+	public void testFindComputerEmpty() {
+		Computer computer = manager.findComputer(1);
+		assertNull(computer);
+	}
+	
+	@Test
+	public void testLCRUD() {
 		Computer computer = new Computer();
 		computer.setName("test");
 		manager.createComputer(computer);
-		List<Computer> computers = manager.listComputer();
-		computer = computers.get(computers.size()-1);
-		System.out.println(computer.toString());
-		computer.setName("test2");
-		computer.setCompany(ManagerFactory.getInstance().getCompanyManager().findCompany(1));
-		manager.updateComputer(computer);
-		computers = manager.listComputer();
-		computer = computers.get(computers.size()-1);
-		System.out.println(computer.toString());
+		List<Computer> listComputer = manager.listComputer();
+		assertEquals(1, listComputer.size());
+		computer = manager.findComputer(listComputer.get(0).getId());
+		assertEquals(listComputer.get(0), computer);
+		assertEquals("test", computer.getName());
+		Computer computerUpdate = new Computer();
+		computerUpdate.setId(computer.getId());
+		computerUpdate.setName("test2");
+		computer = manager.updateComputer(computerUpdate);
+		assertEquals("test2", computer.getName());
 		manager.deleteComputer(computer);
-		computers = manager.listComputer();
-		computer = computers.get(computers.size()-1);
-		System.out.println(computer.toString());
+		assertEquals(0, manager.listComputer().size());
 	}
-
+	
 }
