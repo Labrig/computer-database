239f7a155323f95f850e299cc671fad3acc34f37
Labrig <mgirbal@excilys.com>
2019-02-19 17:36:37 +0100

    [ADD] test for the controller

--- src/main/java/fr/excilys/model/Computer.java
+++ src/main/java/fr/excilys/model/Computer.java
@@ -106,4 +106,62 @@ public class Computer {
 		return "Computer [id=" + this.id + ", name=" + this.name + ", introduced=" + ((this.introduced == null) ? "NA" : this.introduced.toString())
 			+ ", discontinued=" + ((this.discontinued == null) ? "NA" : this.discontinued.toString()) + ", company=" + (this.company == null ? "NA" : this.company.toString()) + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Computer)) {
+			return false;
+		}
+		Computer other = (Computer) obj;
+		if (company == null) {
+			if (other.company != null) {
+				return false;
+			}
+		} else if (!company.equals(other.company)) {
+			return false;
+		}
+		if (discontinued == null) {
+			if (other.discontinued != null) {
+				return false;
+			}
+		} else if (!discontinued.equals(other.discontinued)) {
+			return false;
+		}
+		if (id != other.id) {
+			return false;
+		}
+		if (introduced == null) {
+			if (other.introduced != null) {
+				return false;
+			}
+		} else if (!introduced.equals(other.introduced)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
 }
---
+++ src/test/java/fr/excilys/validator/ComputerValidatorTest.java
@@ -0,0 +1,97 @@
+package fr.excilys.validator;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.junit.Test;
+
+import fr.excilys.exceptions.ComputerFormatException;
+import fr.excilys.model.Computer;
+
+public class ComputerValidatorTest {
+
+	@Test
+	public void testVerifyId() throws ComputerFormatException {
+		Computer computer = new Computer();
+		ComputerValidator.getInstance().verifyId(computer, "10");
+		assertEquals(10, computer.getId());
+	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void testVerifyIdException() throws ComputerFormatException {
+		ComputerValidator.getInstance().verifyId(new Computer(), "test");
+	}
+	
+	@Test
+	public void testVerifyIntro() throws ComputerFormatException, ParseException {
+		Computer computer = new Computer();
+		ComputerValidator.getInstance().verifyIntro(computer, "10-06-2016");
+		assertEquals(new SimpleDateFormat("dd-MM-yyyy").parse("10-06-2016"), computer.getIntroduced());
+	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void testVerifyIntroException1() throws ComputerFormatException {
+		ComputerValidator.getInstance().verifyIntro(new Computer(), "test");
+	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void testVerifyIntroException2() throws ComputerFormatException {
+		ComputerValidator.getInstance().verifyIntro(new Computer(), "10-06");
+	}
+	
+//	@Test(expected = ComputerFormatException.class)
+//	public void testVerifyIntroException3() throws ComputerFormatException {
+//		ComputerValidator.getInstance().verifyIntro(new Computer(), "10-13-2016");
+//	}
+//	
+//	@Test(expected = ComputerFormatException.class)
+//	public void testVerifyIntroException4() throws ComputerFormatException {
+//		ComputerValidator.getInstance().verifyIntro(new Computer(), "00-01-2016");
+//	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void testVerifyDiscoException1() throws ComputerFormatException {
+		ComputerValidator.getInstance().verifyDisco(new Computer(), "test");
+	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void testVerifyDiscoException2() throws ComputerFormatException {
+		ComputerValidator.getInstance().verifyDisco(new Computer(), "10-06");
+	}
+	
+//	@Test(expected = ComputerFormatException.class)
+//	public void testVerifyDiscoException3() throws ComputerFormatException {
+//		ComputerValidator.getInstance().verifyDisco(new Computer(), "10-13-2016");
+//	}
+//	
+//	@Test(expected = ComputerFormatException.class)
+//	public void testVerifyDiscoException4() throws ComputerFormatException {
+//		ComputerValidator.getInstance().verifyDisco(new Computer(), "00-01-2016");
+//	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void verifyIntroBeforeDisco1() throws ComputerFormatException {
+		Computer computer = new Computer();
+		ComputerValidator.getInstance().verifyIntro(computer, "23-01-2019");
+		ComputerValidator.getInstance().verifyDisco(computer, "15-01-2019");
+		ComputerValidator.getInstance().verifyIntroBeforeDisco(computer);
+	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void verifyIntroBeforeDisco2() throws ComputerFormatException {
+		Computer computer = new Computer();
+		ComputerValidator.getInstance().verifyIntro(computer, "15-01-2019");
+		ComputerValidator.getInstance().verifyDisco(computer, "15-01-2019");
+		ComputerValidator.getInstance().verifyIntroBeforeDisco(computer);
+	}
+	
+	@Test(expected = ComputerFormatException.class)
+	public void verifyIntroBeforeDisco3() throws ComputerFormatException {
+		Computer computer = new Computer();
+		ComputerValidator.getInstance().verifyDisco(computer, "15-01-2019");
+		ComputerValidator.getInstance().verifyIntroBeforeDisco(computer);
+	}
+
+}
